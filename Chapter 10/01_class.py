bd12a05df6d2eb069f812b5fea1ed0dcf0c533b5
ThakurAmit47 <asthakur98k@gmail.com>
2026-01-22 16:11:20 +0530

    Create 01_class.py

---
+++ Chapter 10/01_class.py
@@ -0,0 +1,17 @@
+#Example 
+class Employee:
+    language = "python"
+    salary = 30000
+
+Amit = Employee()
+Amit.name = "Amit Singh Rajawat"
+print(Amit.language, Amit.salary, Amit.name)
+# Amit = object
+
+aman = Employee()
+aman.name = "Aman Singh Tomar"
+print(aman.language, aman.salary, aman.name)
+# aman = object
+
+#NOTICE: here name is object attributes.Or salary and language are class attributes as
+# they directly belongs to the class
